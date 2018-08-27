0c6995dde8dfa98a76a0b97f34451e447bd62b49
Quibilia <SirLevalioDaBurnshire@gmail.com>
2018-08-26 21:41:38 -0500

    Adjusted the population limit

--- CCSCommon/World.lua
+++ CCSCommon/World.lua
@@ -630,7 +630,7 @@ return
 						end
 						
 						if parent.popLimit < 1000 then parent.popLimit = 1000 end
-					elseif f1 < 0.05 then
+					elseif f1 < 0.125 then
 						parent.popLimit = math.floor(parent.popLimit + (500 * (0.08 / f1)))
 					end
 				end
