a0257017c8d17d6041300ed48d333c389327451f
Quibilia <SirLevalioDaBurnshire@gmail.com>
2019-02-17 17:53:00 -0600

    Update World.lua

--- CCSCommon/World.lua
+++ CCSCommon/World.lua
@@ -399,7 +399,6 @@ return
 								self.planet[x][y][z].city = ""
 								self.planet[x][y][z].land = false
 								self.planet[x][y][z].neighbors = {}
-								self.planet[x][y][z].hasWaterNeighbors = false
 
 								table.insert(self.planetdefined, {x, y, z})
 							end
@@ -474,19 +473,18 @@ return
 					local y = freeNodes[node][2]
 					local z = freeNodes[node][3]
 					
-					if math.random(1, 5) == math.random(1, 5) then
-						local neighbor = parent:randomChoice(self.planet[x][y][z].neighbors)
-						local nx = neighbor[1]
-						local ny = neighbor[2]
-						local nz = neighbor[3]
-						if self.planet[nx][ny][nz].land == false then
-							self.planet[nx][ny][nz].land = true
-							doneLand = doneLand + 1
-							table.insert(doneNodes, neighbor)
-							local found = false
-							for i, j in pairs(self.planet[nx][ny][nz].neighbors) do if self.planet[j[1]][j[2]][j[3]].land == false then found = true end end
-							if found == true then table.insert(freeNodes, neighbor) end
-						end
+					if math.random(1, 10) == math.random(1, 10) then
+						local neighbor = parent:randomChoice(self.planet[x][y][z].neighbors, true)
+						local nx = self.planet[x][y][z].neighbors[neighbor][1]
+						local ny = self.planet[x][y][z].neighbors[neighbor][2]
+						local nz = self.planet[x][y][z].neighbors[neighbor][3]
+						self.planet[nx][ny][nz].land = true
+						doneLand = doneLand + 1
+						table.insert(doneNodes, self.planet[x][y][z].neighbors[neighbor])
+						local found = false
+						for i, j in pairs(self.planet[nx][ny][nz].neighbors) do if self.planet[j[1]][j[2]][j[3]].land == false then found = true end end
+						if found == true then table.insert(freeNodes, self.planet[x][y][z].neighbors[neighbor]) end
+						table.remove(self.planet[x][y][z].neighbors, neighbor)
 					end
 					
 					local found = false
@@ -494,6 +492,30 @@ return
 					if found == false then table.remove(freeNodes, node) end
 					
 					io.write("\r"..tostring(doneLand).."/"..tostring(maxLand))
+				end
+				
+				for i=1,planetSize do
+					local x = self.planetdefined[i][1]
+					local y = self.planetdefined[i][2]
+					local z = self.planetdefined[i][3]
+					
+					self.planet[x][y][z].neighbors = {}
+
+					for dx=-1,1 do
+						if self.planet[x-dx] ~= nil then
+							for dy=-1,1 do
+								if self.planet[x-dx][y-dy] ~= nil then
+									for dz=-1,1 do
+										if self.planet[x-dx][y-dy][z-dz] ~= nil then
+											if x ~= dx or y ~= dy or z ~= dz then
+												table.insert(self.planet[x][y][z].neighbors, {x-dx, y-dy, z-dz})
+											end
+										end
+									end
+								end
+							end
+						end
+					end
 				end
 
 				print("\nRooting countries...")
