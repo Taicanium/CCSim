257704509220117d9cd9c1f26612695d97437bd3
Quibilia <SirLevalioDaBurnshire@gmail.com>
2019-03-03 18:34:13 -0600

    Update init.lua

--- CCSCommon/init.lua
+++ CCSCommon/init.lua
@@ -1644,7 +1644,6 @@ return
 						end
 						
 						if type(t[k]) == "table" and not t[k].recursed then
-							finished = false
 							t[k].recursed = true
 							self:getRecursiveRefs(t[k], tables)
 							t[k].recursed = nil
