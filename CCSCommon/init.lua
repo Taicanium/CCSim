bb7fae171889eb6d39dce2ead8674bdfc4851efd
Quibilia <SirLevalioDaBurnshire@gmail.com>
2019-03-12 16:01:45 -0500

    Update init.lua

--- CCSCommon/init.lua
+++ CCSCommon/init.lua
@@ -2219,7 +2219,7 @@ return
 					end
 					
 					local rmGens = false
-					if j.royalGenerations == -self.genLimit then for k, l in pairs(j.children) do if l.royalGenerations == -self.genLimit then rmGens = true end end
+					if j.royalGenerations == -self.genLimit then for k, l in pairs(j.children) do if l.royalGenerations == -self.genLimit then rmGens = true end end end
 					if rmGens then
 						j.removed = true
 						for k, l in pairs(j.children) do if not l.royalDescendant then l.royalGenerations = -self.genLimit end end
