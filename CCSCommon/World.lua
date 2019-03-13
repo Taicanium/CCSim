36dcdd5fb4f73068699a0dd6191085d777045805
Quibilia <SirLevalioDaBurnshire@gmail.com>
2019-03-12 20:57:28 -0500

    Update World.lua

--- CCSCommon/World.lua
+++ CCSCommon/World.lua
@@ -434,7 +434,7 @@ return
 							if j < i+1999 then f:write(", ") end
 						end
 
-						f:write(")\ninpdata <- data.frame(X=x, Y=y, Z=z, CSC=csc)\nspheres3d(x=inpdata$X, y=inpdata$Y, z=inpdata$Z, col=inpdata$CSC, size=0.4, xlab=\"\", ylab=\"\", zlab=\"\", box=FALSE, axes=FALSE, top=TRUE, add=TRUE, plot=FALSE")
+						f:write(")\ninpdata <- data.frame(X=x, Y=y, Z=z, CSC=csc)\nshapelist3d(cube3d(), x=inpdata$X, y=inpdata$Y, z=inpdata$Z, col=inpdata$CSC, size=0.32, xlab=\"\", ylab=\"\", zlab=\"\", box=FALSE, axes=FALSE, top=TRUE, add=TRUE, plot=FALSE")
 					else
 						f:write(")\nx <- c(")
 
@@ -485,7 +485,7 @@ return
 							if j < planetSize then f:write(", ") end
 						end
 
-						f:write(")\ninpdata <- data.frame(X=x, Y=y, Z=z, CSC=csc)\nspheres3d(x=inpdata$X, y=inpdata$Y, z=inpdata$Z, col=inpdata$CSC, size=0.4, xlab=\"\", ylab=\"\", zlab=\"\", box=FALSE, axes=FALSE, top=TRUE, add=TRUE, plot=FALSE")
+						f:write(")\ninpdata <- data.frame(X=x, Y=y, Z=z, CSC=csc)\nshapelist3d(cube3d(), x=inpdata$X, y=inpdata$Y, z=inpdata$Z, col=inpdata$CSC, size=0.32, xlab=\"\", ylab=\"\", zlab=\"\", box=FALSE, axes=FALSE, top=TRUE, add=TRUE, plot=FALSE")
 					end
 				end
 
