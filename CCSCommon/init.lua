249d0756ca9fdeaf7dff989dec4476837991d9fb
Quibilia <SirLevalioDaBurnshire@gmail.com>
2019-03-13 09:44:01 -0500

    Update init.lua

--- CCSCommon/init.lua
+++ CCSCommon/init.lua
@@ -2174,13 +2174,14 @@ return
 				if i then
 					local set = i.gensSet
 					i.gensSet = true
-					if g ~= 1 and v < 0 then i.royalDescendant = true end
+					if g ~= 1 and v < 0 then i.royalDescendant = true
+					elseif g == 1 and v > 0 then i.royalAncestor = true end
 					if not set then
 						if v > -1 and v < i.royalGenerations then i.royalGenerations = v end
-						if i.royalDescendant and v < 0 then i.royalGenerations = -2 end
-						if g ~= 1 then for j, k in pairs(i.children) do self:setGens(k, v+1, 1) end end
-						if i.royalGenerations == -2 then self:setGens(i.father, -2, 0) elseif not d then self:setGens(i.father, v-1, 1) end
-						if i.royalGenerations == -2 then self:setGens(i.mother, -2, 0) elseif not d then self:setGens(i.mother, v-1, 1) end
+						if i.royalDescendant and i.royalAncestor then i.royalGenerations = -2 end
+						for j, k in pairs(i.children) do self:setGens(k, v+1, 1) end
+						if i.royalGenerations == -2 then self:setGens(i.father, -2, 0) else self:setGens(i.father, v-1, 0) end
+						if i.royalGenerations == -2 then self:setGens(i.mother, -2, 0) else self:setGens(i.mother, v-1, 0) end
 					end
 					i.gensSet = false
 				end
