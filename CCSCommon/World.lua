9f89447c4e4a4f3bac0a226b0b5a9b6f544c0bca
Quibilia <sirlevaliodaburnshire@gmail.com>
2019-08-04 19:42:58 +0200

    If a country is defeated in war four times within 20 years, it will now be automatically conquered by the last country which defeated it.

--- CCSCommon/World.lua
+++ CCSCommon/World.lua
@@ -666,6 +666,16 @@ return
 						cp:event(parent, "Disappeared")
 						for j=1,#parent.c_events do if parent.c_events[j].name == "Conquer" then cp:triggerEvent(parent, j, true) end end
 					end
+
+					local defCount = 0
+					local lastDef = ""
+					for j=#cp.events,1,-1 do if cp.events[j].Year > parent.years-20 and cp.events[j].Event:match("Defeat in war") then
+						defCount = defCount+1
+						if lastDef == "" then lastDef = cp.events[j].Event:gsub("Defeat in war with ", "") end
+					end end
+					if defCount > 3 then
+						for j, k in pairs(self.countries) do if k.name == lastDef then for j=1,#parent.c_events do if parent.c_events[j].name == "Conquer" then cp:triggerEvent(parent, j) end end end end
+					end
 				end end
 
 				self.gPop = 0
