d55e8924c17e2617f56ce8953531d59383c97c9a
Quibilia <SirLevalioDaBurnshire@gmail.com>
2019-08-12 17:53:52 -0500

    Separated land and water border checking for efficiency.

--- CCSCommon/Country.lua
+++ CCSCommon/Country.lua
@@ -100,16 +100,20 @@ return
 							local x2, y2, z2 = table.unpack(node1.neighbors[j])
 							local node2 = parent.thisWorld.planet[x2][y2][z2]
 							if node2.country == other.name then return 2 end
-							if node2.land == false and border < 1 then
-								for k=1,#parent.thisWorld.planetdefined do
-									local x3, y3, z3 = table.unpack(parent.thisWorld.planetdefined[k])
-									local node3 = parent.thisWorld.planet[x3][y3][z3]
-									if node3.country == other.name then for l=1,#node3.neighbors do
-										local x4, y4, z4 = table.unpack(node3.neighbors[l])
-										local node4 = parent.thisWorld.planet[x4][y4][z4]
-										if node4.land == false then border = 1 end
-									end end
-								end
+							if node2.land == false and border < 1 then border = 1 end
+						end
+					end
+				end
+				if border == 1 then
+					border = 0
+					for i=1,#parent.thisWorld.planetdefined do
+						local x, y, z = table.unpack(parent.thisWorld.planetdefined[i])
+						local node1 = parent.thisWorld.planet[x][y][z]
+						if node1.country == other.name then
+							for j=1,#node1.neighbors do
+								local x2, y2, z2 = table.unpack(node1.neighbors[j])
+								local node2 = parent.thisWorld.planet[x2][y2][z2]
+								if node2.land == false then return 1 end
 							end
 						end
 					end
--- CCSCommon/Region.lua
+++ CCSCommon/Region.lua
@@ -31,18 +31,22 @@ return
 							local node2 = parent.thisWorld.planet[x2][y2][z2]
 							if node2.region == other.name and border < 4 then return 4 end
 							if node2.country == other.name and border < 3 then border = 3 end
-							if node2.land == false and border < 2 then
-								for k=1,#parent.thisWorld.planetdefined do
-									local x3, y3, z3 = table.unpack(parent.thisWorld.planetdefined[k])
-									local node3 = parent.thisWorld.planet[x3][y3][z3]
-									if node3.region == other.name or node3.country == other.name then for l=1,#node3.neighbors do
-										local x4, y4, z4 = table.unpack(node3.neighbors[l])
-										local node4 = parent.thisWorld.planet[x4][y4][z4]
-										if node4.land == false then
-											if node3.region == other.name and border < 2 then border = 2 end
-											if node3.country == other.name and border < 1 then border = 1 end
-										end
-									end end
+							if node2.land == false and border < 2 then border = 2 end
+						end
+					end
+				end
+				if border == 2 then
+					border = 0
+					for i=1,#parent.thisWorld.planetdefined do
+						local x, y, z = table.unpack(parent.thisWorld.planetdefined[i])
+						local node1 = parent.thisWorld.planet[x][y][z]
+						if node1.region == other.name or node1.country == other.name then
+							for j=1,#node1.neighbors do
+								local x2, y2, z2 = table.unpack(node1.neighbors[j])
+								local node2 = parent.thisWorld.planet[x2][y2][z2]
+								if node2.land == false then
+									if node1.region == other.name then return 2 end
+									if node1.country == other.name then border = 1 end
 								end
 							end
 						end
