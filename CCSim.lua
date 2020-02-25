59cb3801cfc010c5178f200b5a819bba0d27dc7e
Quibilia <SirLevalioDaBurnshire@gmail.com>
2020-02-25 10:38:40 -0600

    Update CCSim.lua

--- CCSim.lua
+++ CCSim.lua
@@ -6,32 +6,31 @@ if not CCSMStatus or not CCSModule then error(tostring(CCSModule)) os.exit(1) en
 CCSFStatus, CCSCommon = pcall(CCSModule)
 if not CCSFStatus or not CCSCommon then error(tostring(CCSCommon)) os.exit(1) end
 
-function main()
-	CCSCommon:rseed()
-	if _DEBUG then
-		testGlyphs()
-		testNames()
-	end
-	while _RUNNING do
-		UI:clear()
-		UI:printf("\n\n\tCCSIM : Compact Country Simulator\n\n")
-		if _DEBUG then UI:printf("\t-- DEBUG MODE ENABLED --\n\n") end
-		UI:printf("MAIN MENU\n\n1\t-\tBegin a new simulation.")
-		UI:printf("2\t-\tReview the output of a previous simulation.")
-		if _DEBUG then UI:printf("3\t-\tExecute a line of Lua code.\n") end
-		UI:printf("Q\t-\tExit the program.")
-		UI:printp("\n > ")
+CCSCommon:rseed()
 
-		local datin = UI:readl()
-		if datin == "1" then simNew() _RUNNING = false
-		elseif datin == "2" then simReview()
-		elseif datin == "3" and _DEBUG then debugLine()
-		elseif datin:lower() == "q" then _RUNNING = false end
-	end
-
-	CCSCommon = nil
-	if cursesstatus then curses.endwin() end
+if _DEBUG then
+	testGlyphs()
+	testNames()
 end
 
-main()
+while _RUNNING do
+	UI:clear()
+	UI:printf("\n\n\tCCSIM : Compact Country Simulator\n\n")
+	if _DEBUG then UI:printf("\t-- DEBUG MODE ENABLED --\n\n") end
+	UI:printf("MAIN MENU\n\n1\t-\tBegin a new simulation.")
+	UI:printf("2\t-\tReview the output of a previous simulation.")
+	if _DEBUG then UI:printf("3\t-\tExecute a line of Lua code.\n") end
+	UI:printf("Q\t-\tExit the program.")
+	UI:printp("\n > ")
+
+	local datin = UI:readl()
+	if datin == "1" then simNew() _RUNNING = false
+	elseif datin == "2" then simReview()
+	elseif datin == "3" and _DEBUG then debugLine()
+	elseif datin:lower() == "q" then _RUNNING = false end
+end
+
+CCSCommon = nil
+if cursesstatus then curses.endwin() end
+
 os.exit(0)
